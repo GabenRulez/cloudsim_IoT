638cb96525ddae887ceb3fa21d5796579d493385
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2020-12-30 21:03:17 +0100

    Now the program doesn't crash for directories containing space sign (' ').

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/planetlab/Dvfs.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/power/planetlab/Dvfs.java
@@ -33,7 +33,7 @@ public class Dvfs {
 	public static void main(String[] args) throws IOException {
 		boolean enableOutput = true;
 		boolean outputToFile = false;
-		String inputFolder = Dvfs.class.getClassLoader().getResource("workload/planetlab").getPath();
+		String inputFolder = Dvfs.class.getClassLoader().getResource("workload/planetlab").getPath().replace("%20", " ");
 		String outputFolder = "output";
 		String workload = "20110303"; // PlanetLab workload
 		String vmAllocationPolicy = "dvfs"; // DVFS policy without VM migrations
