15fe6d118f732ddf17b1a561351268d76a9980d2
Piotr Kasprzyk <kaspiotr@gmail.com>
2021-01-05 07:17:43 +0100

    Add renewable energy source calculation
       * use equation E = A x r x H x PR
       * additionally add logging info about used grid power
       * note: consider using BigDecimals instead of doubles in calculations

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/PhotovoltaicFarm.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/PhotovoltaicFarm.java
@@ -2,14 +2,22 @@ package org.cloudbus.cloudsim.power;
 
 public class PhotovoltaicFarm {
 
-    //TODO: find and apply a correct equation, find needed parameters
+    public double calculateEnergy(double timeDelta, double radiation) {
+        // In order to get energy produced by solar panels we used following equation: E = A x r x H x PR
+        // where:
+        int numberOfPanels = 10;
+        // A - total panel area [m^2]
+        int panelArea = 10;
+        // r - solar panel yield [%]
+        double solarPanelYield = 0.8;
+        // H assumption: instead of annual average solar radiation on tilted panel average solar radiation
+        // in given period of time was used
+        // PR - performance ratio, constant for losses (range between 0.5 and 0.9, default value = 0.75)
+        double performanceRatio = 0.75;
 
-    private int numberOfPanels = 10;
-    private int panelArea = 10;
-    private float performanceRatio = .75f;
-
-    public float calculateEnergy(float timeDelta, float radiation){
-        return 0;
+        double averageSolarRadiationInGivenPeriodOfTime = radiation / timeDelta;
+        double powerInKiloWh = panelArea * numberOfPanels * solarPanelYield * averageSolarRadiationInGivenPeriodOfTime * performanceRatio;
+        return 3600000 * powerInKiloWh; // power in W*sec = J
     }
 
 
--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/RenewableEnergySource.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/RenewableEnergySource.java
@@ -1,16 +1,36 @@
 package org.cloudbus.cloudsim.power;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class RenewableEnergySource {
+
+    static DateTimeFormatter DATE_TIME_FORMATTER_NO_SECONDS = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+    static BSRNDataLoader bsrnDataLoader;
+    static {
+        try {
+            bsrnDataLoader = new BSRNDataLoader();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    static LinkedHashMap<String, Float> radiationsByDate = bsrnDataLoader.getRadiationsByDate();
+    static List<String> keys = new ArrayList<>(radiationsByDate.keySet());
+    static List<Float> values = new ArrayList<>(radiationsByDate.values());
+    static PhotovoltaicFarm photovoltaicFarm = new PhotovoltaicFarm();
     /*
-        TODO:
-        - get data from BSRNDataLoader and apply it to the PhotovoltaicFarm
-        - set start date (equivalent of 0 timestamp, this is arbitrary choice made by us)
-        - implement getEnergyInTimeframe function.
+        DONE:
+        - get data from BSRNDataLoader and apply it to the PhotovoltaicFarm (done)
+        - set start date (equivalent of 0 timestamp, this is arbitrary choice made by us) (done)
+        - implement getEnergyInTimeFrame function (done).
         Remember that data from BSRNDataLoader is in minute intervals, and timestamps are seconds.
         For example, if currentTime = 90 and timeDiff = 60, we need to get half energy from first minute and half energy from second minute.
         If currentTime = 180 and timeDiff = 60, we just need to get whole energy from third minute.
     */
-
     /**
      * Gets the energy in given timeframe.
      *
@@ -18,7 +38,59 @@ public class RenewableEnergySource {
      * @param timeDiff interval length [s]
      * @return energy in [currentTime - timeDiff, currentTime] interval
      */
-    double getEnergyInTimeframe(double currentTime, double timeDiff) {
-        return 0.0;
+    double getEnergyInTimeFrame(double currentTime, double timeDiff) {
+        double start = currentTime - timeDiff;
+        String startKey = getStartKey((int) Math.floor(start));
+        String endKey = getEndKey((int) Math.ceil(currentTime));
+        System.out.println(startKey);
+        System.out.println(endKey);
+        double roundedUpSum = values.subList(keys.indexOf(startKey), keys.indexOf(endKey) + 1).stream()
+                .mapToDouble(Float::doubleValue)
+                .sum();
+        double startEnergyDelta = computeStartEnergyDelta(startKey, start);
+        double endEnergyDelta = computeEndEnergyDelta(endKey, currentTime);
+        double radiation = roundedUpSum - startEnergyDelta - endEnergyDelta;
+        return photovoltaicFarm.calculateEnergy(timeDiff, radiation);
     }
+
+    private static double computeEndEnergyDelta(String endKey, double end) {
+        float energy = radiationsByDate.get(endKey);
+        double deltaTime = 60 - (end - (Math.floor(end / 60.0) * 60.0));
+        System.out.println(deltaTime);
+        return energy * (deltaTime / 60.0);
+    }
+
+    private static double computeStartEnergyDelta(String startKey, double start) {
+        float energy = radiationsByDate.get(startKey);
+        double deltaTime = start - (Math.floor(start / 60.0) * 60.0);
+        System.out.println(deltaTime);
+        return energy * (deltaTime / 60.0);
+    }
+
+    private static String getStartKey(int start) {
+        LocalDateTime timeStamp = getTimestamp(start);
+        timeStamp = timeStamp.withSecond(0);
+        return timeStamp.format(DATE_TIME_FORMATTER_NO_SECONDS);
+    }
+
+    private static String getEndKey(int end) {
+        LocalDateTime timeStamp = getTimestamp(end);
+        timeStamp = timeStamp.plusMinutes(1).withSecond(0);
+        return timeStamp.format(DATE_TIME_FORMATTER_NO_SECONDS);
+    }
+
+    private static LocalDateTime getTimestamp(int start) {
+        LocalDateTime startDateTime = LocalDateTime.parse(radiationsByDate.keySet().iterator().next(), DATE_TIME_FORMATTER_NO_SECONDS);
+        return startDateTime.plusSeconds(start);
+    }
+
+    // below method has been created only for test purposes
+//    public static void main(String[] args) {
+//        RenewableEnergySource renewableEnergySource = new RenewableEnergySource();
+//        double currentTime = 2400.01;
+//        double timeDiff = 300.0;
+//        double radiation = renewableEnergySource.getEnergyInTimeFrame(currentTime, timeDiff);
+//        double result = photovoltaicFarm.calculateEnergy(timeDiff, radiation);
+//        System.out.println(result);
+//    }
 }
