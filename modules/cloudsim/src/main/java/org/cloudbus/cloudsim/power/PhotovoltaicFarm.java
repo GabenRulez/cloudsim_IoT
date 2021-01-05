ae9e580657493f9b234943a3fd5644962d126336
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-01-06 00:34:59 +0100

    Added PhotovoltaicFarm constructors.

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/PhotovoltaicFarm.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/power/PhotovoltaicFarm.java
@@ -1,24 +1,60 @@
 package org.cloudbus.cloudsim.power;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PhotovoltaicFarm {
+
+
+    private final int numberOfPanels;
+    private final int panelArea;
+    private final double solarPanelYield;
+    private final double performanceRatio;
+
+    public PhotovoltaicFarm(int numberOfPanels, int panelArea, double solarPanelYield, double performanceRatio){
+        this.numberOfPanels = numberOfPanels;
+        this.panelArea = panelArea;
+        this.solarPanelYield = solarPanelYield;
+        this.performanceRatio = performanceRatio;
+
+        checkIfParametersAreWrong();
+    }
+
+    public PhotovoltaicFarm(){
+        System.out.println("PhotovoltaicFarm constructor called without parameters.");
+
+        this.numberOfPanels = ThreadLocalRandom.current().nextInt(1,25);
+        this.panelArea = ThreadLocalRandom.current().nextInt(1,5);
+        this.solarPanelYield = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
+        this.performanceRatio = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
+        checkIfParametersAreWrong();
+
+        System.out.printf("Class got random attributes 'numberOfPanels'=%d 'panelArea'=%d 'solarPanelYield'=%g 'performanceRatio'=%g%n", numberOfPanels, panelArea, solarPanelYield, performanceRatio);
+    }
+
 
     public double calculateEnergy(double timeDelta, double radiation) {
         // In order to get energy produced by solar panels we used following equation: E = A x r x H x PR
         // where:
-        int numberOfPanels = 10;
+    //int numberOfPanels = 10;
         // A - total panel area [m^2]
-        int panelArea = 10;
+    //int panelArea = 10;
         // r - solar panel yield [%]
-        double solarPanelYield = 0.8;
+    //double solarPanelYield = 0.8;
         // H assumption: instead of annual average solar radiation on tilted panel average solar radiation
         // in given period of time was used
         // PR - performance ratio, constant for losses (range between 0.5 and 0.9, default value = 0.75)
-        double performanceRatio = 0.75;
+    //double performanceRatio = 0.75;
 
         double averageSolarRadiationInGivenPeriodOfTime = radiation / timeDelta;
         double powerInKiloWh = panelArea * numberOfPanels * solarPanelYield * averageSolarRadiationInGivenPeriodOfTime * performanceRatio;
         return 3600000 * powerInKiloWh; // power in W*sec = J
     }
 
-
+    private void checkIfParametersAreWrong(){
+        String errorString = "Arguments passed to PhotovoltaicFarm constructor were incorrect.";
+        if(numberOfPanels < 0)                                  throw new IllegalArgumentException(errorString + " 'numberOfPanels' shouldn't be lesser than 0. Got '" + numberOfPanels + "'.");
+        if(panelArea < 0)                                       throw new IllegalArgumentException(errorString + " 'panelArea' shouldn't be lesser than 0. Got '" + panelArea + "'.");
+        if(solarPanelYield < 0.0 || solarPanelYield > 1.0)      throw new IllegalArgumentException(errorString + " 'solarPanelYield' should be between 0.0 and 1.0. Got '" + solarPanelYield + "'.");
+        if(performanceRatio < 0.0 || performanceRatio > 1.0)    throw new IllegalArgumentException(errorString + " 'performanceRatio' should be between 0.0 and 1.0. Got '" + performanceRatio + "'.");
+    }
 }
